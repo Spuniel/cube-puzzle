df613c36858c8b95f956514661bebd280a8af7b6
Spuniel <115584361+Spuniel@users.noreply.github.com>
2025-04-14 18:38:37 +0200

    end

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,56 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'cube-puzzle';
+  index: number[] = Array.from({ length: 16 }, (_, i) => i + 1);
+  temp: number = 0;
+
+  text: string = "Cube Puzzle";
+
+  shuffle(){
+    this.index = this.index.sort(() => Math.random() - 0.5);
+  }
+
+  move(event: Event): void {
+    var element = event.target as HTMLImageElement;
+    var alt = Number(element.alt);
+    var emptyIndex = this.index.indexOf(16);
+
+    // lewo
+    if (alt > 0 && alt - 1 === emptyIndex) {
+      this.swap(alt, emptyIndex);
+    }
+    // prawo
+    else if (alt < 15 && alt + 1 === emptyIndex) {
+      this.swap(alt, emptyIndex);
+    }
+    // góra
+    else if (alt > 3 && alt - 4 === emptyIndex) {
+      this.swap(alt, emptyIndex);
+    }
+    // dół
+    else if (alt < 12 && alt + 4 === emptyIndex) {
+      this.swap(alt, emptyIndex);
+    }
+
+    this.checkIfWin();
+  }
+
+  swap(i: number, j: number): void {
+    [this.index[i], this.index[j]] = [this.index[j], this.index[i]];
+  }
+
+  checkIfWin(){
+    var count = 0;
+    var t = this.index[0];
+    for(var i = 0; i < 16; i++){
+      if(i < this.index[i]) 
+      {
+        count++
+      };
+    }
+    console.log(count.toString());
+    if(count == 16){
+      this.text = "WIN";
+    }
+  }
 }
